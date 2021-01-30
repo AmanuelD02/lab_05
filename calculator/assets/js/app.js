ca592ec04fabb2c03d99dcd30d49341c9962816e
Amanuel <amanueldebebe1000@gmail.com>
2021-01-30 19:40:23 +0300

    Added button listener to calculators

--- calculator/assets/js/app.js
+++ calculator/assets/js/app.js
@@ -1,14 +1,21 @@
+// define UI variables
+
+var input1 = document.getElementById('Input1');
+var input2 = document.getElementById('Input2');
+var result = document.getElementById('result');
 var calculator = {
-	add: (values) => {
-		return values.reduce((x, y) => x + y);
+	add: (val1, val2) => {
+		return val1 + val2;
 	},
 	subs: (var1, var2) => {
 		return var1 - var2;
 	},
-	mult: (values) => {
-		return values.reduce((x, y) => x * y);
+	mult: (val1, val2) => {
+		return val1 * val2;
 	},
 	div: (var1, var2) => {
+		if (var2 == 0) return '∞';
+
 		return var1 / var2;
 	},
 	max: (values) => {
@@ -27,7 +34,41 @@ var calculator = {
 	},
 	sqrt: (value) => {
 		return Math.sqrt(value);
+	},
+	pow: (val1, val2) => {
+		return Math.pow(val1, val2);
 	}
 };
 
-function calc(operand) {}
+function calc(operand) {
+	console.log(operand);
+	var var1 = input1.value ? parseInt(input1.value) : 0;
+	var var2 = input2.value ? parseInt(input2.value) : 0;
+
+	if (operand == '+') {
+		input1.value = result.innerHTML = calculator.add(var1, var2);
+		input2.value = '';
+	} else if (operand == '*') {
+		input1.value = result.innerHTML = calculator.mult(var1, var2);
+		input2.value = '';
+	} else if (operand == 'Sqrt') {
+		cls();
+		result.innerHTML = calculator.sqrt(var1);
+	} else if (operand == 'Pow') {
+		cls();
+		result.innerHTML = calculator.pow(var1, var2);
+	} else if (operand == '-') {
+		cls();
+		result.innerHTML = calculator.subs(var1, var2);
+	} else if (operand == '/') {
+		cls();
+		result.innerHTML = calculator.div(var1, var2);
+	}
+}
+
+function cls() {
+	console.log('CLEAR');
+	input1.value = '';
+	input2.value = '';
+	result.innerHTML = '0';
+}
